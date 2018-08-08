b85841df692f40ff926baa7658a21f25473528dc
Kyler Lopez <klopez.2021@saintannsny.org>
2018-08-08 14:02:21 +0000

    newest

--- js/profile.js
+++ js/profile.js
@@ -1,25 +1,23 @@
 window.onload = getProfile()
+let profileName = document.querySelector("#profileName")
+let profileSchool = document.querySelector("#profileSchool")
+let profileEmail = document.querySelector("#profileEmail")
 
 function getProfile() {
     firebase.database().ref('/profiles').on('value', (snapshot) => {
         let data = snapshot.val()
         for (key in data) {
+            console.log(key)
+            console.log(data[key].email)
             // console.log(data[key].firstName)
-            if (localStorage.getItem("email") === data[key].email) {
-                console.log("yeet")
-                let yourProfile = data[].filter(data[key].email === localStorage.getItem("email"))
-                console.log(yourProfile)
-                
+            if (localStorage.getItem("email") == data[key].email) {
+                console.log(localStorage.getItem("email"))
+                profileName.innerHTML = `${data[key].firstName} ${data[key].lastName}`
+                profileSchool.innerHTML = `${data[key].schoolName} `
+                profileEmail.innerHTML = `${data[key].email} `
             }
 
-            // displayPostings.innerHTML += `
-            // <div class="jumbotron jumboBoi">
-            //     <h1 class="text-uppercase text-white">${data[key].title}</h1>
-            //     <hr class="my-4">
-            //     <p class="text-white">${data[key].description}</p>
-            //     <p c1ass="text-white" style="color:white">${data[key].email}</p>
-            // </div>
-            // `
+
         }
     })
 }
